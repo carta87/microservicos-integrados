1eed109b054f84f297f1a70968cf521d4a96dd26
CarlosTafurR <153922372+CarlosTafurR@users.noreply.github.com>
2024-11-25 17:02:37 -0500

    se agrega validacion de campos OWASP

--- microservice-report/src/main/java/com/microservice_report/MicroserviceReportApplication.java
+++ microservice-report/src/main/java/com/microservice_report/MicroserviceReportApplication.java
@@ -1,10 +1,15 @@
 package com.microservice_report;
 
+import org.owasp.html.PolicyFactory;
+import org.owasp.html.Sanitizers;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import static java.lang.System.*;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -15,11 +20,27 @@ public class MicroserviceReportApplication {
 		SpringApplication.run(MicroserviceReportApplication.class, args);
 	}
 
-	/*@Bean
+	@Bean
 	CommandLineRunner init(){
 		return args -> {
+			String prueba = "<script>alert('hola mundo')</script>$final";
+
+			//prueba = String.valueOf(prueba.contains("<"));
+
+			String caracterInvalid = "<";
+
+			if(prueba.contains(caracterInvalid)){
+                out.println("El texto contiene etiquetas HTML");
+            } else {
+                out.println("El texto no contiene etiquetas HTML");
+            }
+
+			// Sanitizar el texto para evitar ataques XSS
+			PolicyFactory policyFactory = Sanitizers.FORMATTING;
+			String sanitizedText = policyFactory.sanitize(prueba);
+			out.println("salida"+sanitizedText);
 
 		};
-	}*/
+	}
 
 }
